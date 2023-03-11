e96aa2db28c6be1c6dc7b5df91fb9fcb44e657c3
Gennady Badinov <gennady.badinov@gmail.com>
2023-03-11 12:28:39 +0300

    feat: Create broker service entrypoint

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting broker service at http://127.0.0.1:%s\n", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.router(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
