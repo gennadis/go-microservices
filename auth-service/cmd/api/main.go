86f0b28cecde3a6a29783fdf6f36b93846c12500
Gennady Badinov <gennady.badinov@gmail.com>
2023-03-11 19:47:15 +0300

    feat: Set up stub auth service

--- auth-service/cmd/api/main.go
+++ auth-service/cmd/api/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"auth/data"
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 const webPort = "80"
@@ -13,5 +16,17 @@ type Config struct {
 }
 
 func main() {
+	log.Println("Starting authentication service")
 
+	// TODO connect to DB
+	app := Config{}
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
 }
